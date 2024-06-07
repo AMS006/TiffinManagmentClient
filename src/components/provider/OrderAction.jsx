4ba8038f1d8be376a99bce0956cf1cb6babcd036
Anas Sain <98727007+AMS006@users.noreply.github.com>
2024-06-07 18:23:38 +0530

    Update OrderAction.jsx

--- src/components/provider/OrderAction.jsx
+++ src/components/provider/OrderAction.jsx
@@ -22,7 +22,7 @@ export default function OrderActionMenu({ order }) {
   useEffect(() => {
     if (order)
       dispatch(updateOrder({ _id: order._id, status, user: order.user, provider: order.provider, food: order.food }))
-  }, [dispatch,status,order])
+  }, [status])
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget)
   };
